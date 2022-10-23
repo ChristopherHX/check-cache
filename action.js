9a0e10d432eac88cec080dd7de9f22113ce6596e
Christopher Homberger <christopher.homberger@web.de>
2022-10-23 19:30:01 +0200

    Fix missing inputs

--- action.js
+++ action.js
@@ -4,12 +4,14 @@ var utils = require("@actions/cache/lib/internal/cacheUtils");
 
 (async () => {
     try {
+        const key = core.getInput("key");
+        const path = core.getInput("path");
         const compressionMethod = await utils.getCompressionMethod();
         var cacheResponse = await cacheHttpClient.getCacheEntry([key], [path], { compressionMethod });
         if(cacheResponse && cacheResponse.archiveLocation) {
             core.setOutput("cache-hit", "true");
         }
     } catch {
-        
+
     }
 })();
