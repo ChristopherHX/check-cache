9ec871d4d714b30d3cff450d101e62698a66212a
Christopher Homberger <christopher.homberger@web.de>
2022-10-23 19:26:31 +0200

    Initial version

---
+++ action.js
@@ -0,0 +1,15 @@
+var core = require("@actions/core");
+var cacheHttpClient = require("@actions/cache/lib/internal/cacheHttpClient");
+var utils = require("@actions/cache/lib/internal/cacheUtils");
+
+(async () => {
+    try {
+        const compressionMethod = await utils.getCompressionMethod();
+        var cacheResponse = await cacheHttpClient.getCacheEntry([key], [path], { compressionMethod });
+        if(cacheResponse && cacheResponse.archiveLocation) {
+            core.setOutput("cache-hit", "true");
+        }
+    } catch {
+        
+    }
+})();
